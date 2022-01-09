7e8a3f0d17a7922a4a60b893c0b044bc48be71c0
Aljamil1113 <aarazasaljamil@gmail.com>
2022-01-09 20:31:16 +0800

    Selectors

--- SampSelenium/SampSelenium/EntryPoint.cs
+++ SampSelenium/SampSelenium/EntryPoint.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using System.Threading;
+using System;
 
 namespace SampSelenium
 {
@@ -10,13 +11,69 @@ namespace SampSelenium
     {
         static void Main()
         {
+            //IWebDriver driver = new FirefoxDriver();
+
+            //driver.Navigate().GoToUrl("http://testing.todorvachev.com");
+
+            //Thread.Sleep(3000);
+
+            //driver.Quit();
+        
+            //****************SELECTORS***************//
             IWebDriver driver = new ChromeDriver();
 
-            driver.Navigate().GoToUrl("http://testing.todorvachev.com");
+            //string url = "http://testing.todorvachev.com/selectors/name/";
+            //string url = "http://testing.todorvachev.com/selectors/id/";
+            //string url = "http://testing.todorvachev.com/selectors/class-name/";
+            string url = "https://testing.todorvachev.com/css-path/";
+            string cssPath = "#post-108 > div > figure > img";
+            string xPath = "//*[@id=\"post-108\"]/div/figure/img";
 
-            Thread.Sleep(3000);
+            driver.Navigate().GoToUrl(url);
+
+            //IWebElement element = driver.FindElement(By.Name("myName"));
+            //IWebElement element = driver.FindElement(By.Id("testImage"));
+            //IWebElement element = driver.FindElement(By.ClassName("testClass"));
+
+            IWebElement cssPathElement = driver.FindElement(By.CssSelector(cssPath));
+            IWebElement xPathElement = driver.FindElement(By.XPath(xPath));
+
+            if (cssPathElement.Displayed)
+            {
+                GreenMessage("Yes! I can see the element, it's right there!!! (" + cssPathElement.Text + ").");
+            }
+            else
+            {
+                RedMessage("Well, something went wrong, I couldn't see the element!");
+            }
+
+            if (xPathElement.Displayed)
+            {
+                GreenMessage("Yes! I can see the element, it's right there!!! (" + xPathElement.Text + ").");
+            }
+            else
+            {
+                RedMessage("Well, something went wrong, I couldn't see the element!");
+            }
+
+            Thread.Sleep(5000);
 
             driver.Quit();
         }
+
+        private static void RedMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static void GreenMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
     }
 }

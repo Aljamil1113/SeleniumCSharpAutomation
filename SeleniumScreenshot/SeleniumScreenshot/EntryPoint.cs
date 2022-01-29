4e1a1103867405c3db0dde689d37ca16253b0fc6
Aljamil1113 <aarazasaljamil@gmail.com>
2022-01-29 22:49:50 +0800

    Sample getting screenshot in Selenium

--- SeleniumScreenshot/SeleniumScreenshot/EntryPoint.cs
+++ SeleniumScreenshot/SeleniumScreenshot/EntryPoint.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.IO;
 
 namespace SeleniumScreenshot
 {
@@ -8,6 +9,21 @@ namespace SeleniumScreenshot
         static void Main()
         {
             IWebDriver chrome = new ChromeDriver();
+
+            string screenshotsDirectory = Directory.GetCurrentDirectory() + @"\screenshots";
+
+            chrome.Navigate().GoToUrl("http://google.com");
+
+            Screenshot googleScreenshot = ((ITakesScreenshot)chrome).GetScreenshot();
+
+            if(!Directory.Exists(screenshotsDirectory))
+            {
+                Directory.CreateDirectory(Directory.GetCurrentDirectory() + @"\screenshots");
+            }
+
+            googleScreenshot.SaveAsFile(Directory.GetCurrentDirectory() + @"\screenshots\googlescreenshot.png", ScreenshotImageFormat.Png);
+
+            chrome.Quit();
         }
     }
 }

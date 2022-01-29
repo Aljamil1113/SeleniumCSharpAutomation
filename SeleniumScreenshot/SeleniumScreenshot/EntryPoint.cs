bd11f748b1660a4802fbc69bec15b4f6a4eb0663
Aljamil1113 <aarazasaljamil@gmail.com>
2022-01-29 22:18:31 +0800

    New Project SeleniumScreenshot

---
+++ SeleniumScreenshot/SeleniumScreenshot/EntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumScreenshot
+{
+    class EntryPoint
+    {
+        static void Main()
+        {
+            IWebDriver chrome = new ChromeDriver();
+        }
+    }
+}

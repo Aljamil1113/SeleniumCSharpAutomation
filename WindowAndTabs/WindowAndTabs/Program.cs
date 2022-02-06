faef94e8864b9840237e1ae75246be5667d76069
Aljamil1113 <aarazasaljamil@gmail.com>
2022-02-06 19:32:22 +0800

    Type text in search from NewTab

--- WindowAndTabs/WindowAndTabs/Program.cs
+++ WindowAndTabs/WindowAndTabs/Program.cs
@@ -1,10 +1,50 @@
 ﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
 namespace WindowAndTabs
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            List<string> handles = new List<string>();
+            IWebDriver driver = new ChromeDriver();
+
+            IWebElement newTab;
+            IWebElement newWindow;
+
+            string url = "https://testing.todorvachev.com/new-tab/";
+            string newTabsSelector = ".entry-content > p:nth-child(1) > a:nth-child(1)";
+            string newWindowSelector = ".entry-content > p:nth-child(1) > a:nth-child(3)";
+
+            driver.Navigate().GoToUrl(url);
+
+            newTab = driver.FindElement(By.CssSelector(newTabsSelector));
+            newWindow = driver.FindElement(By.CssSelector(newWindowSelector));
+
+            newTab.Click();
+
+            handles = driver.WindowHandles.ToList();
+
+            IWebElement usernameBox = driver.FindElement(By.Name("username"));
+
+            usernameBox.SendKeys("selenium");
+
+            Thread.Sleep(1000);
+
+            driver.SwitchTo().Window(handles[1]);
+
+            System.Console.WriteLine(driver.CurrentWindowHandle);
+            System.Console.WriteLine(driver.CurrentWindowHandle);
+
+            IWebElement searchBox1 = driver.FindElement(By.Name("q"));
+            searchBox1.SendKeys("Selenium");
+
+
         }
     }
 }

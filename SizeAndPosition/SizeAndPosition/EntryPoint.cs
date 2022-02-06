0404d72a4032b7eafce2b00db51f58f3dcb756c5
Aljamil1113 <aarazasaljamil@gmail.com>
2022-02-06 15:59:12 +0800

    Size and postion sample automate

--- SizeAndPosition/SizeAndPosition/EntryPoint.cs
+++ SizeAndPosition/SizeAndPosition/EntryPoint.cs
@@ -1,11 +1,27 @@
 ﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Drawing;
+
 namespace SizeAndPosition
 {
     class EntryPoint
     {
         static void Main(string[] args)
         {
-            
+            Size size = new Size();
+            size.Width =  600;
+            size.Height = 600;
+
+            Point position = new Point();
+            position.X = -10;
+            position.Y = -10;
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Size = size;
+            driver.Manage().Window.Position = position;
+
+            driver.Navigate().GoToUrl("http://testing.todvachev.com");
         }
     }
 }

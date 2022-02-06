e056eed855b036222ea02b778b90d8f49267102c
Aljamil1113 <aarazasaljamil@gmail.com>
2022-02-06 14:16:02 +0800

    Working with elements

---
+++ ElementLocation/ElementLocation/EntryPoint.cs
@@ -0,0 +1,14 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ElementLocation
+{
+    public class EntryPoint
+    {
+        static void Main()
+        {
+           
+            
+        }
+    }
+}

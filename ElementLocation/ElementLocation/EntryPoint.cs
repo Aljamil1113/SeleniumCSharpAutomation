5f0bb7fd03c3a4a8e442558b8ceeff471cf9e6e8
Aljamil1113 <aarazasaljamil@gmail.com>
2022-02-06 14:45:52 +0800

    Get Location of Web Element

--- ElementLocation/ElementLocation/EntryPoint.cs
+++ ElementLocation/ElementLocation/EntryPoint.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
 
 namespace ElementLocation
 {
@@ -7,8 +8,25 @@ namespace ElementLocation
     {
         static void Main()
         {
-           
+            IWebDriver driver = new ChromeDriver();
+
+            string url = "http://testing.todvachev.com";
+
+            driver.Navigate().GoToUrl(url);
             
+            IWebElement image = driver.FindElement(By.CssSelector(".alignnone"));
+
+            driver.Manage().Window.Maximize();
+
+            Console.WriteLine(image.Location.X);
+            Console.WriteLine(image.Location.Y);
+            Console.WriteLine(image.Size.Width);
+            Console.WriteLine(image.Size.Height);
+            
+
+            //driver.Quit();
         }
+
+        
     }
 }
